ccf038b2291c4d19eac297bf85e91470477ff9bc
harikrushna <harikrushna3443@gmail.com>
2024-07-07 04:22:17 +0530

    first page of test

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ module.exports = {
     buildActivity: false,
   },
   images: {
-    domains: ["images.unsplash.com","s3-alpha-sig.figma.com"],
+    domains: ["images.unsplash.com", "s3-alpha-sig.figma.com", 'i.ibb.co',
+      'scontent.fotp8-1.fna.fbcdn.net'],
+    unoptimized: true,
   },
   experimental: {
     serverActions: true,
